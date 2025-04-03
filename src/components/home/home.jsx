ad0222a8d7b74be8c5e1942bd8a77b4dada19bca
Davi Curty <davicurty850@gmail.com>
2025-04-03 11:05:46 -0300

    feat: finishing the estruturation of aplication

---
+++ src/components/home/home.jsx
@@ -0,0 +1,7 @@
+import "./home.css";
+
+const Home = () => {
+  return <></>;
+};
+
+export default Home;
--- src/page/App.jsx
+++ src/page/App.jsx
@@ -1,7 +1,22 @@
+import "./App.css";
+import Header from "../components/header/header";
+import Home from "../components/home/home";
+import About from "../components/about/about";
+import Projects from "../components/projects/projects";
+import Contact from "../components/contact/contact";
+
 const App = () => {
   return (
     <>
-      <h1>hello</h1>
+      <div className="header">
+        <Header />
+      </div>
+      <div className="body">
+        <Home />
+        <About />
+        <Projects />
+        <Contact />
+      </div>
     </>
   );
 };
