948cd2c61846d81699505b6ceea483ce4fc12135
Davi Curty <davicurty850@gmail.com>
2025-04-03 13:02:05 -0300

    chore: style of the header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,36 @@
+import { DownloadSimple } from "@phosphor-icons/react";
 import "./header.css";
 
 const Header = () => {
-  return <></>;
+  return (
+    <>
+      <div className="headers">
+        <div className="logo">
+          Porti<span>folio</span>
+        </div>
+        <div className="links">
+          <ul className="">
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Projects</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div className="cv">
+          <p>Download</p>
+          <DownloadSimple size={20} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
