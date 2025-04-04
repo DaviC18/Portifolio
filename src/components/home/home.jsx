e31b65dae1df90dc9fa7708644cc092db28a7c00
Davi Curty <davicurty850@gmail.com>
2025-04-04 19:15:03 -0300

    chore: finishing the header

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,7 +1,20 @@
 import "./home.css";
+import Me from "../../assets/111199405.png";
 
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <div className="me">
+        <div className="photo">
+          <img src={Me} alt="" />
+        </div>
+        <div className="info">
+          <p className="name">Davi Curty</p>
+          <p className="dev">Desenvolvedor Full-stack</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
